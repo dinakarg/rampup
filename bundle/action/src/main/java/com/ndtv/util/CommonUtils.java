b6803d5c361dfd4033929ae38d08dd54b21ac5ba
Rashmi <srashmi@virtusa.com>
2015-09-11 14:49:45 +0530

    util class for constants

--- bundle/action/src/main/java/com/ndtv/util/CommonUtils.java
+++ bundle/action/src/main/java/com/ndtv/util/CommonUtils.java
@@ -3,8 +3,11 @@
  */
 package com.ndtv.util;
 
+import java.util.List;
+import java.util.ArrayList;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Value;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,49 +19,72 @@ import org.slf4j.LoggerFactory;
  */
 public class CommonUtils {
 
-	/** The Constant EMPTY_STRING. */
-	private static final String EMPTY_STRING = "";
+    /** The Constant EMPTY_STRING. */
+    private static final String EMPTY_STRING = "";
 
-	/** The Constant LOG. */
-	private static final Logger LOG = LoggerFactory
-			.getLogger(CommonUtils.class);
+    /** The Constant LOG. */
+    private static final Logger LOG = LoggerFactory
+            .getLogger(CommonUtils.class);
 
-	/**
-	 * Gets the node property value.
-	 * 
-	 * @param node
-	 *            the node
-	 * @param propertyName
-	 *            the property name
-	 * @return the node property value
-	 */
-	public static String getNodePropertyValue(final Node node,
-			final String propertyName) {
-		String propertyValue = null;
-		try {
-			if (node.hasProperty(propertyName)) {
-				propertyValue = node.getProperty(propertyName).getString();
-			}
-		} catch (RepositoryException e) {
-			LOG.error(
-					"Error while retrieving the property value from the node "
-							+ e.getMessage(), e);
-		}
-		return propertyValue;
+    /**
+     * Gets the node property value.
+     * 
+     * @param node
+     *            the node
+     * @param propertyName
+     *            the property name
+     * @return the node property value
+     */
+    public static String getNodePropertyValue(final Node node,
+            final String propertyName) {
+        String propertyValue = null;
+        try {
+            if (node.hasProperty(propertyName)) {
+                propertyValue = node.getProperty(propertyName).getString();
+            }
+        } catch (RepositoryException e) {
+            LOG.error(
+                    "Error while retrieving the property value from the node "
+                            + e.getMessage(), e);
+        }
+        return propertyValue;
 
-	}
+    }
 
-	/**
-	 * Return empty if null.
-	 * 
-	 * @param propertyValue
-	 *            the property value
-	 * @return the string
-	 */
-	public static String returnEmptyIfNull(final String propertyValue) {
-		if (propertyValue == null) {
-			return EMPTY_STRING;
-		}
-		return propertyValue.trim();
-	}
+    public static List<String> getNodePropertyValues(final Node node,
+            final String propertyName) {
+        Value[] propertyValues = {};
+        List<String> listOfValues = null;
+        try {
+            propertyValues = node.getProperty(propertyName).getValues();
+            if (propertyValues.length > 0) {
+                listOfValues = new ArrayList<String>();
+                for (Value str : propertyValues) {
+                    listOfValues.add(str.toString());
+                }
+            }
+
+        } catch (RepositoryException e) {
+            LOG.error(
+                    "Error while retrieving the property value from the node "
+                            + e.getMessage(), e);
+        }
+
+        return listOfValues;
+    }
+
+    /**
+     * Return empty if null.
+     * 
+     * @param propertyValue
+     *            the property value
+     * @return the string
+     */
+    public static String returnEmptyIfNull(final String propertyValue) {
+        if (propertyValue == null) {
+            return EMPTY_STRING;
+        }
+        return propertyValue.trim();
+    }
+
 }
